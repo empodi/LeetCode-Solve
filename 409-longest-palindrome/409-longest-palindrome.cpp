2569f671b352d31df5d96d04454ca5df3b49d8c2
seonghwan <76815545+empodi@users.noreply.github.com>
2022-01-15 21:52:13 +0900

    Time: 9 ms (7.90%), Space: 6.7 MB (13.70%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.cpp
+++ 409-longest-palindrome/409-longest-palindrome.cpp
@@ -3,17 +3,22 @@ public:
     int longestPalindrome(string s) {
         
         int len = 0, ans = 0;
+        bool isOne = false;
         vector<int> v(256, 0);
         
         for (auto& c : s) v[c]++;
         
-        for (auto& elem : v) if (elem) len++;
+        for (auto& elem : v) {
+            if (elem) len++;
+            if (elem == 1) isOne = true;
+        }
         
         for (int i = 0; i < len; i++) {
             int pos = max_element(v.begin(), v.end()) - v.begin();
             if (v[pos] % 2 == 1 && v[pos] > 1) {
                 ans += v[pos] - 1;
                 v[pos] = 1;
+                isOne = true;
             }
             else if (v[pos] % 2 == 0) {
                 ans += v[pos];
@@ -21,11 +26,7 @@ public:
             }
         }
         
-        for (auto& elem : v) 
-            if (elem) {
-                ans++;
-                break;
-            }
+        if (isOne) ans++;
         
         return ans;
     }
