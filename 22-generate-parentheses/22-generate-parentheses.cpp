403af92ffd00250a5cf503f874fec8867db0e907
seonghwan <76815545+empodi@users.noreply.github.com>
2022-01-17 13:56:18 +0900

    Time: 8 ms (35.18%), Space: 11.4 MB (81.88%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
     int len;
-    void helper(vector<string>& p, string str, int left, int right) {
+    void helper(vector<string>& p, string& str, int left, int right) {
         
         if (left == len && right == len) {
             p.push_back(str);
             return;
         }
         if (left == right) {
-            helper(p, str + "(", left + 1, right);
+            str += "(";
+            helper(p, str, left + 1, right);
+            str.pop_back();
             return;
         }
-        if (left < len)
-            helper(p, str + "(", left + 1, right);
-        if (right < len)
-            helper(p, str + ")", left, right + 1);
+        if (left < len) {
+            str += "(";
+            helper(p, str, left + 1, right);
+            str.pop_back();
+        }
+        if (right < len) {
+            str += ")";
+            helper(p, str, left, right + 1);
+            str.pop_back();
+        }
     }
     
     vector<string> generateParenthesis(int n) {
