0d66b3e2482aa0d96c298e637e63b9dabfd2caeb
seonghwan <76815545+empodi@users.noreply.github.com>
2022-02-04 16:41:41 +0900

    Time: 3 ms (49.52%), Space: 7.9 MB (5.87%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -7,17 +7,13 @@ public:
         if (len == 1) return nums[0];
         if (len == 2) return max(nums[0], nums[1]);
         
-        vector<vector<int>> dp(2, vector<int>(len, 0));
+        vector<int> dp(len);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
         
-        dp[0][0] = dp[1][0] = nums[0];
-        dp[0][1] = nums[1];
-        dp[1][1] = max(nums[0], nums[1]);
+        for (int i = 2; i < len; i++)
+            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1]);
         
-        for (int i = 2; i < len; i++) {
-            dp[0][i] += max(dp[0][i - 2], dp[1][i - 2]) + nums[i];
-            dp[1][i] = max(dp[0][i - 1], dp[1][i - 1]); 
-        }
-        
-        return max(dp[0][len - 1], dp[1][len - 1]);
+        return dp[len - 1];
     }
 };
