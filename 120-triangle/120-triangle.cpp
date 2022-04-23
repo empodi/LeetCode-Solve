184a53539e2b0588dc5bff5a3a47bf60c5e31989
seonghwan <76815545+empodi@users.noreply.github.com>
2022-04-23 20:24:44 +0900

    Time: 4 ms (91.34%), Space: 8.5 MB (90.69%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -2,16 +2,32 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         
+        if (triangle.size() == 1) return triangle[0][0];
+        
         int len = triangle.size();
         
-        for (int i = len - 2; i >= 0; i--) {
+        for (int i = 1; i < len; i++) {
             
             for (int j = 0; j <= i; j++) {
                 
-                triangle[i][j] += min(triangle[i + 1][j], triangle[i + 1][j + 1]);
+                if (j == 0) {
+                    triangle[i][j] += triangle[i - 1][j];
+                }
+                else if (j == i) {
+                    triangle[i][j] += triangle[i - 1][j - 1];
+                }
+                else {
+                    triangle[i][j] += min(triangle[i - 1][j], triangle[i - 1][j - 1]);
+                }
             }
         }
         
-        return triangle[0][0];
+        int ret = 111111;
+        
+        for (int i = 0; i < len; i++) {
+            ret = min(ret, triangle[len - 1][i]);
+        }
+        
+        return ret;
     }
 };
