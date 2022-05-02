3ed6fd53912b3b732adb5591705fc5b72b232570
seonghwan <76815545+empodi@users.noreply.github.com>
2022-05-02 13:09:32 +0900

    Time: 158 ms (71.06%), Space: 17.5 MB (32.90%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.py
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -1,42 +1,34 @@
 class Solution:
-    # 파이썬 함수 argument???
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         
         maxArea = 0
-        
         height = len(grid)
         width = len(grid[0])
+        
         moving = [[0,1],[0,-1],[1,0],[-1,0]]
         
+        def solve_dfs(x, y) -> int:
+            
+            if grid[x][y] == 0: return 0
+            
+            grid[x][y] = 0
+            
+            retArea = 1
+            
+            for mov in moving:
+                nx = x + mov[0]
+                ny = y + mov[1]
+                
+                if nx < 0 or ny < 0 or nx >= height or ny >= width:
+                    continue
+                
+                retArea += solve_dfs(nx, ny)
+            
+            return retArea
+            
         for i in range(height):
             for j in range(width):
-                
-                if grid[i][j] == 0: continue
-                
-                grid[i][j] = 0
-                
-                localArea = 1
-                queue = deque([(i, j)])
-                
-                while queue:
-                    elem = queue.popleft()
-                    x = elem[0]
-                    y = elem[1]
-                    
-                    for k in moving:
-                        nx = k[0] + x
-                        ny = k[1] + y
-                        
-                        if nx < 0 or ny < 0 or nx >= height or ny >= width:
-                            continue
-                        if grid[nx][ny] == 0: 
-                            continue
-                        
-                        grid[nx][ny] = 0
-                        queue.append((nx, ny))
-                        
-                        localArea = localArea + 1
-                
-                maxArea = max(maxArea, localArea)
-                
+                if grid[i][j] == 1:
+                    maxArea = max(maxArea, solve_dfs(i, j))        
+
         return maxArea
