0e69d7644549d7f04a4f29a1c40dba7f7a6bdef9
seonghwan <76815545+empodi@users.noreply.github.com>
2022-05-04 17:07:50 +0900

    Time: 422 ms (47.59%), Space: 16.3 MB (80.34%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.py
@@ -0,0 +1,41 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        
+        height = len(grid)
+        width = len(grid[0])
+        
+        moving = [ [1,0], [-1,0], [0,1], [0,-1] ]
+        
+        answer = 0
+        
+        for i in range(height):
+            for j in range(width):
+                
+                if grid[i][j] == "0":
+                    continue
+                    
+                answer += 1
+                
+                queue = [[i, j]]
+                
+                while queue:
+                    x, y = (queue[0][0], queue[0][1])
+                    queue.pop(0)
+                    
+                    grid[x][y] = "0"
+                    
+                    for mov in moving:
+                        nx, ny = (x + mov[0], y + mov[1])
+                
+                        if nx < 0 or ny < 0 or nx >= height or ny >= width:
+                            continue
+                        if grid[nx][ny] == "0":
+                            continue
+                            
+                        grid[nx][ny] = "0"
+                        queue.append([nx, ny])
+                    
+        return answer
+            
+            
+        
