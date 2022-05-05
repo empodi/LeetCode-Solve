cc4419c72ba8a2ad82e8f79cf0af47908a773ce5
seonghwan <76815545+empodi@users.noreply.github.com>
2022-05-05 13:31:59 +0900

    Time: 1229 ms (62.45%), Space: 25 MB (39.92%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,16 +1,15 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         
-        minBuy = 987654321
+        minBuy = prices[0]
         maxProfit = 0
         
         for price in prices:
             
             if price < minBuy:
                 minBuy = price
-                continue
-            
-            maxProfit = maxProfit if price - minBuy < maxProfit else price - minBuy
+            else:            
+                maxProfit = maxProfit if price - minBuy < maxProfit else price - minBuy
                 
         return maxProfit
             
