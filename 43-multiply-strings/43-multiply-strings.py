d2ccdb3a640f9b4fd57ff1897654f18ff736a028
seonghwan <76815545+empodi@users.noreply.github.com>
2022-05-06 17:20:35 +0900

    Time: 369 ms (7.67%), Space: 13.9 MB (74.86%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.py
+++ 43-multiply-strings/43-multiply-strings.py
@@ -4,38 +4,43 @@ class Solution:
         if num1 == "0" or num2 == "0":
             return "0"
         
-        if len(num1) < len(num2):
-            num1, num2 = num2, num1
-            
         num1Len, num2Len = len(num1), len(num2)
+        
+        if num1Len < num2Len:
+            num1, num2 = num2, num1
+            num1Len, num2Len = num2Len, num1Len
+            
         answer = None
         
-        def digitMultiply(multiplicand: str, digit: str):
+        def digitMultiply(multiplicand: str, digit: str) -> str:
+            mIdx = len(multiplicand) - 1
             carry = 0
             result = ''
             
-            for m in reversed(multiplicand):
-                carry = carry + int(m) * int(digit)
+            while mIdx >= 0 or carry:
+                if mIdx >= 0:
+                    carry = carry + int(multiplicand[mIdx]) * int(digit)
                 result += str(carry % 10)
                 carry //= 10
-            
-            if carry:
-                result += str(carry)
+                mIdx -= 1
             
             return result[::-1]
         
-        def strSum(a, b):
-            a, b = list(a), list(b)
+        def strSum(a: str, b: str) -> str:
+            aIdx, bIdx = len(a) - 1, len(b) - 1
             carry = 0
             result = ''
             
-            while a or b or carry:
-                if a:
-                    carry += int(a.pop())
-                if b:
-                    carry += int(b.pop())
+            while aIdx >= 0 or bIdx >= 0 or carry:
+                if aIdx >= 0:
+                    carry += int(a[aIdx])
+                if bIdx >= 0:
+                    carry += int(b[bIdx])
+                    
                 result += str(carry % 10)
                 carry //= 10
+                aIdx -= 1
+                bIdx -= 1
             
             return result[::-1]
         
