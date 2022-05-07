66364fe278abcf0fd6320a901871930f265d9a2f
seonghwan <76815545+empodi@users.noreply.github.com>
2022-05-07 14:17:08 +0900

    Time: 109 ms (37.29%), Space: 14.4 MB (48.24%) - LeetHub

---
+++ 47-permutations-ii/47-permutations-ii.py
@@ -0,0 +1,35 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        
+        nums.sort()
+        numsLen = len(nums)
+        
+        visited = [False for i in range(numsLen)]
+        answer = []
+        
+        def solve(array): # no return
+            
+            if len(array) == numsLen:
+                answer.append(array.copy())
+                
+            for i in range(0, numsLen):
+                
+                if visited[i]:
+                    continue
+                if i and nums[i] == nums[i - 1] and not visited[i - 1]:
+                    continue
+                
+                array.append(nums[i])
+                visited[i] = True
+                
+                solve(array)
+                array.pop()
+                visited[i] = False
+                
+        solve([])
+        
+        return answer
+            
+            
+            
+            
