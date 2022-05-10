c030f1e02aa0f8cdd6978cd0592d359d3321f512
seonghwan <76815545+empodi@users.noreply.github.com>
2022-05-10 16:33:46 +0900

    Time: 103 ms (87.58%), Space: 17.3 MB (76.66%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -2,18 +2,18 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
         myHash = dict()
-        retArray = []
+        answer = []
+        rIdx = 0
         
         for word in strs:
             tmp = "".join(sorted(word))
 
             if tmp not in myHash:
-                myHash[tmp] = [word]
+                myHash[tmp] = rIdx
+                answer.append([word])
+                rIdx += 1
             else:
-                myHash[tmp].append(word)
-                
-        for key in myHash:
-            retArray.append(myHash[key])
+                answer[myHash[tmp]].append(word)
             
-        return retArray
+        return answer
         
