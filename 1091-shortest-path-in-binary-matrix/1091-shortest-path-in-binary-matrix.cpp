ac042c6b62ab3b71dd31a5700e81bc11e6221db9
seonghwan <76815545+empodi@users.noreply.github.com>
2022-05-16 10:52:51 +0900

    Time: 115 ms (44.27%), Space: 20.1 MB (59.11%) - LeetHub

---
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -0,0 +1,52 @@
+typedef pair<pair<int,int>,int> pii;
+
+class Solution {
+public:
+    bool visited[101][101];
+    int height;
+    int width;
+    
+    int moving[8][2] = { {1,0},{1,1},{1,-1},{-1,0},{-1,1},{-1,-1},{0,1},{0,-1} };
+    
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        
+        height = grid.size();
+        width = grid[0].size();
+        int answer = INT_MAX;
+        
+        queue<pii> q;
+        
+        if (grid[0][0] == 0)
+            q.push({{0,0},1});
+        
+        while (!q.empty()) {
+            
+            int x = q.front().first.first;
+            int y = q.front().first.second;
+            int dist = q.front().second;
+            q.pop();
+            
+            if (x == height - 1 && y == width - 1) {
+                answer = answer < dist ? answer : dist;
+                continue;
+            }
+            
+            visited[x][y] = true;
+            
+            for (int k = 0; k < 8; k++) {
+                int nx = x + moving[k][0];
+                int ny = y + moving[k][1];
+                
+                if (nx < 0 || ny < 0 || nx >= height || ny >= width) 
+                    continue;
+                if (grid[nx][ny] == 1 || visited[nx][ny]) 
+                    continue;
+                
+                visited[nx][ny] = true;
+                q.push({{nx, ny}, dist + 1});
+            }
+        }
+        
+        return answer = (answer == INT_MAX) ? -1: answer;
+    }
+};
