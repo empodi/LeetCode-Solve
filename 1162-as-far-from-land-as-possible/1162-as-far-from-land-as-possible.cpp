0813c4704120b268b7a61efd1d420e2d34b19b5c
seonghwan <76815545+empodi@users.noreply.github.com>
2022-05-18 17:10:47 +0900

    Time: 76 ms (69.64%), Space: 19.8 MB (82.73%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -1,42 +1,9 @@
-#define MAX 987654321
 class Solution {
 private:
     int height;
     int width;
     bool visited[101][101];
     int moving[4][2] = { {1,0},{-1,0},{0,1},{0,-1} };
-    
-    void BFS(vector<vector<int>>& grid, int i, int j) {
-        
-        queue<pair<pair<int,int>,int>> q;
-        q.push({{i, j}, 1});
-        visited[i][j] = true;
-        
-        while (!q.empty()) {
-            
-            int x = q.front().first.first;
-            int y = q.front().first.second;
-            int dist = q.front().second;
-            q.pop();
-            
-            if (grid[x][y] > dist) continue;
-            
-            for (int k = 0; k < 4; k++) {
-                int nx = x + moving[k][0];
-                int ny = y + moving[k][1];
-                
-                if (nx < 0 || ny < 0 || nx >= height || ny >= width)
-                    continue;
-                if (visited[nx][ny] || grid[nx][ny] < dist)
-                    continue;
-                
-                visited[nx][ny] = true;
-                grid[nx][ny] = dist;
-                q.push({{nx, ny}, dist + 1});
-            }
-        }
-    }
-    
 public:
     int maxDistance(vector<vector<int>>& grid) {
         
@@ -45,35 +12,44 @@ public:
         
         int answer = 0;
         int cnt = 0;
+        queue<pair<int,int>> q;
         
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
-                if (grid[i][j]) 
+                if (grid[i][j]) {
                     cnt++;
-                grid[i][j] = grid[i][j] ? -1 : MAX;
+                    q.push({i,j});
+                }
             }
         }
         
         if (cnt == 0 || cnt == height * width)
             return -1;
-        
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                if (grid[i][j] < 0) {
-                    memset(visited, false, sizeof(visited));
-                    BFS(grid, i, j);
+
+        while (!q.empty()) {
+            
+            int qLen = q.size();
+            answer++;
+            
+            while (qLen--) {
+                
+                int x = q.front().first;
+                int y = q.front().second;
+                q.pop();
+                
+                for (int i = 0; i < 4; i++) {
+                    int nx = x + moving[i][0];
+                    int ny = y + moving[i][1];
+                    
+                    if (nx < 0 || ny < 0 || nx >= height || ny >= width || grid[nx][ny])
+                        continue;
+                    
+                    grid[nx][ny] = 1;
+                    q.push({nx, ny});
                 }
             }
         }
-                    
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                if (grid[i][j] < 0) 
-                    continue;
-                answer = max(answer, grid[i][j]);
-            }
-        }
         
-        return answer;
+        return answer - 1;
     }
 };
