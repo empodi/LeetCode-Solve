0e7585671521499136ebf41b5db87158fb3cfb53
seonghwan <76815545+empodi@users.noreply.github.com>
2022-05-19 14:53:06 +0900

    Time: 8 ms (48.65%), Space: 14.5 MB (46.47%) - LeetHub

--- 865-smallest-subtree-with-all-the-deepest-nodes/865-smallest-subtree-with-all-the-deepest-nodes.cpp
+++ 865-smallest-subtree-with-all-the-deepest-nodes/865-smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -11,26 +11,33 @@
  */
 class Solution {
 public:
-    int findDepth(TreeNode* node) {
-        
-        if (!node->left && !node->right) return 0;
-        
-        int ld = node->left ? findDepth(node->left) : 0;
-        int rd = node->right ? findDepth(node->right) : 0;
-        return 1 + max(ld, rd);
-    }
+    int depth[501];
     
-    TreeNode* subtreeWithAllDeepest(TreeNode* root, int curDepth = 0, int targetDepth = 0) {
+    TreeNode* subtreeWithAllDeepest(TreeNode* root, int curDepth = 0) {
         
-        if (!targetDepth) 
-            targetDepth = findDepth(root);
-        if (curDepth == targetDepth || !root) 
-            return root;
+        depth[root->val] = curDepth;
         
-        TreeNode* left = subtreeWithAllDeepest(root->left, curDepth + 1, targetDepth);
-        TreeNode* right = subtreeWithAllDeepest(root->right, curDepth + 1, targetDepth);
-     
-        if (left && right) return root;
+        TreeNode *left = nullptr, *right = nullptr;
+        int lv = 0, rv = 0;
+        
+        if (root->left) {
+            left = subtreeWithAllDeepest(root->left, curDepth + 1);
+            lv = depth[root->left->val];
+        }
+        if (root->right) {
+            right = subtreeWithAllDeepest(root->right, curDepth + 1);
+            rv = depth[root->right->val];
+        }
+        
+        if (!left && !right) return root;
+
+        depth[root->val] = max(lv, rv);
+        
+        if (left && right){
+            if (lv == rv) return root;
+            else if (lv > rv) return left;
+            else return right;
+        }
         if (left) return left;
         return right;
     }
