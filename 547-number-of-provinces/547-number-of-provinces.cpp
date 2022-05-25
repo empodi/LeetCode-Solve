9d4ea61baaa6e6a903756ad9234112aca54685c0
seonghwan <76815545+empodi@users.noreply.github.com>
2022-05-25 16:59:24 +0900

    Time: 35 ms (42.07%), Space: 13.7 MB (63.77%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -1,24 +1,34 @@
 class Solution {
+private:
+    int parent[210];
 public:
-    //bool visited[201];
-    void check(vector<vector<int>>& A, int city) {
-        for (int i = 0; i < A[city].size(); i++) {
-            if (!A[city][i]) continue;
-            A[city][i] = 0;
-            check(A, i);
-        }
+    int Find(int x) {
+        if (x == parent[x]) return x;
+        return parent[x] = Find(parent[x]);
     }
+
     int findCircleNum(vector<vector<int>>& isConnected) {
     
         int len = isConnected.size();
-        int answer = 0;
+        int cnt = 0;
+        
+        for (int i = 0; i <= len; i++) 
+            parent[i] = i;
         
         for (int i = 0; i < len; i++) {
-            if (isConnected[i][i]) {
-                check(isConnected, i);
-                answer++;
+            for (int j = 0; j < len; j++) {
+                if (i == j || !isConnected[i][j]) 
+                    continue;
+                
+                int x = Find(i);
+                int y = Find(j);
+                isConnected[x][y] = isConnected[y][x] = 0;
+                
+                if (x != y) cnt++;
+                parent[x] = y;
             }
         }
-        return answer;
+        
+        return len - cnt;
     }
 };
