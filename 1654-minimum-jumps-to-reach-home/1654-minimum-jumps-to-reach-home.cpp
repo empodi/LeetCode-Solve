9c84d9e03106e01f28e277fc423d823d762bb30c
seonghwan <76815545+empodi@users.noreply.github.com>
2022-06-01 12:57:49 +0900

    Time: 90 ms (34.07%), Space: 17.9 MB (82.92%) - LeetHub

---
+++ 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.cpp
@@ -0,0 +1,43 @@
+typedef pair<int,int> pii; // cur, prev <= positive when prev jump was heading forward
+class Solution {
+private:
+    int a;
+    int b;
+    int x;
+    bool visited[2000000];
+    int solve() {
+        
+        queue<pair<int,int>> q;
+        q.push({0,0});
+        
+        while (q.size() > 0) {
+            int cur = q.front().first;
+            int jmp = q.front().second;
+            q.pop();
+            
+            if (cur == x) {
+                return abs(jmp);
+            }
+            if (cur > 20000) continue;
+            if (visited[cur]) continue;
+            visited[cur] = true;
+            
+            if (jmp >= 0 && cur - b > 0) 
+                q.push({cur - b, -(jmp + 1)});
+            
+            q.push({cur + a, abs(jmp) + 1});
+        }
+        return -1;
+    }
+public:
+    int minimumJumps(vector<int>& forbidden, int _a, int _b, int _x) {
+        a = _a;
+        b = _b;
+        x = _x;
+        memset(visited, false, sizeof(visited));
+        for (int i = 0; i < forbidden.size(); i++)
+            visited[forbidden[i]] = true;
+        
+        return solve();
+    }
+};
