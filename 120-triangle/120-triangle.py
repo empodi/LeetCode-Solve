22f472a85a8febfcc2082dc2aa981af87a264921
seonghwan <76815545+empodi@users.noreply.github.com>
2022-06-13 09:39:51 +0900

    Time: 68 ms (92.19%), Space: 15 MB (37.66%) - LeetHub

--- 120-triangle/120-triangle.py
+++ 120-triangle/120-triangle.py
@@ -2,17 +2,15 @@ class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         
         tLen = len(triangle)
-        
         if (tLen == 1): return triangle[0][0];
         
         for i, row in enumerate(triangle):
-            
-            if i == 0: continue;
-            
+            if i == 0: continue
             for j, val in enumerate(row):
-                
-                if j == 0: row[j] += triangle[i - 1][j]
-                elif i == j: row[j] += triangle[i - 1][j - 1]
+                if j == 0: 
+                    row[j] += triangle[i - 1][j]
+                elif i == j: 
+                    row[j] += triangle[i - 1][j - 1]
                 else: 
                     row[j] += min(triangle[i - 1][j - 1], triangle[i - 1][j])
                     
