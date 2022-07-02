28e006f149add3549c442943c673880161d8dab6
seonghwan <76815545+empodi@users.noreply.github.com>
2022-07-02 12:08:46 +0900

    Time: 75 ms (95.88%), Space: 32.4 MB (40.04%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
@@ -1,36 +1,19 @@
 #define MOD 1000000007
 class Solution {
 public:
-    int maxArea(int h, int w, vector<int>& horizontalCuts, vector<int>& verticalCuts) {
-        
+    int maxArea(int h, int w, vector<int>& horizontalCuts, vector<int>& verticalCuts) {       
         sort(horizontalCuts.begin(), horizontalCuts.end());
         sort(verticalCuts.begin(), verticalCuts.end());
         
-        int maxHeight = 0, maxWidth = 0;
+        int maxHeight = max(horizontalCuts[0], h - horizontalCuts.back()); 
+        int maxWidth = max(verticalCuts[0], w - verticalCuts.back());
         
-        for (int i = 0; i < horizontalCuts.size(); i++) {
-            if (i == 0) {
-                maxHeight = max(maxHeight, horizontalCuts[i]);
-            }
-            else {
-                maxHeight = max(maxHeight, horizontalCuts[i] - horizontalCuts[i - 1]);
-            }
-            if (i == horizontalCuts.size() - 1) {
-                maxHeight = max(maxHeight, h - horizontalCuts[i]);
-            }
+        for (int i = 1; i < horizontalCuts.size(); i++) {
+            maxHeight = max(maxHeight, horizontalCuts[i] - horizontalCuts[i - 1]);
         }
-        for (int i = 0; i < verticalCuts.size(); i++) {
-            if (i == 0) {
-                maxWidth = max(maxWidth, verticalCuts[i]);
-            }
-            else {
-                maxWidth = max(maxWidth, verticalCuts[i] - verticalCuts[i - 1]);
-            }
-            if (i == verticalCuts.size() - 1) {
-                maxWidth = max(maxWidth, w - verticalCuts[i]);
-            }
+        for (int i = 1; i < verticalCuts.size(); i++) {
+            maxWidth = max(maxWidth, verticalCuts[i] - verticalCuts[i - 1]);
         }
-
         return ((long)maxWidth * maxHeight) % MOD;
     }
 };
