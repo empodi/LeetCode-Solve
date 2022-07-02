7c99494e3df512ddeb56d9b4941565758b954ab5
seonghwan <76815545+empodi@users.noreply.github.com>
2022-07-02 14:02:28 +0900

    Time: 0 ms (100.00%), Space: 6.9 MB (91.29%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+        int maxLen = 0, start = 0, end = 0;
+        int ASC[128] = { 0, };
         
-        bool ASC[128];
-        memset(ASC, false, sizeof(ASC));
-        
-        int localLen = 0, maxLen = 0, prevIdx = 0;
-        
-        for (int i = 0; i < s.length(); i++) {
-            
-            char c = s[i];
-            
-            if (ASC[c] == true) {
-                localLen = 1;
-                memset(ASC, false, sizeof(ASC));
-                
-                prevIdx++;
-                ASC[s[prevIdx]] = true;
-                i = prevIdx;
+        while (end < s.length()) {
+            if (ASC[s[end]] > 0) {
+                while (start < end) {
+                    ASC[s[start++]]--;
+                    if (ASC[s[end]] == 0) break;
+                }
             }
-            else {
-                localLen++;
-                maxLen = maxLen > localLen ? maxLen : localLen;
-                ASC[c] = true;
-            }
+            maxLen = max(maxLen, end - start + 1);
+            ASC[s[end]]++;
+            end++;
         }
-        
         return maxLen;
     }
 };
