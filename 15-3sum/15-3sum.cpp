e0e87d55c3683f5c224a540ce76708ba90b88b36
seonghwan <76815545+empodi@users.noreply.github.com>
2022-07-02 17:58:46 +0900

    Time: 118 ms (67.01%), Space: 19.9 MB (87.70%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -7,6 +7,7 @@ public:
         
         for (int i = 0; i < nums.size() - 2; i++) {
             if (i && nums[i] == nums[i - 1]) continue;
+            
             int left = i + 1, right = nums.size() - 1;
             while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
@@ -19,11 +20,9 @@ public:
                 }
                 else if (sum > 0) {
                     right--;
-                    while (left < right && nums[right] == nums[right + 1]) right--;
                 }
                 else {
                     left++;
-                    while (left < right && nums[left] == nums[left - 1]) left++;
                 }
             }
         }
