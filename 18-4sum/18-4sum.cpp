9e75077227fe5cb064843e09e4a097db0ce9fd25
seonghwan <76815545+empodi@users.noreply.github.com>
2022-07-03 15:13:04 +0900

    Time: 84 ms (66.28%), Space: 13.2 MB (50.40%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -1,32 +1,28 @@
-class Solution {
+class Solution {    // O(N^3)
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        sort(nums.begin(), nums.end());
+        if (nums.size() < 4) return {};
+        sort(nums.begin(),nums.end());
         vector<vector<int>> ret;
-        int len = nums.size();
         
-        for (int i = 0; i < len - 3; i++) {
+        for (int i = 0; i < nums.size() - 3; i++) {
             if (i && nums[i] == nums[i - 1]) continue;
             if (nums[i] > 0 && nums[i] > target) break;
-            for (int j = i + 1; j < len - 2; j++) {
+            for (int j = i + 1; j < nums.size() - 2; j++) {
                 if (j > i + 1 && nums[j] == nums[j - 1]) continue;
-                int twoSum = nums[i] + nums[j];
-                if (twoSum > 0 && twoSum > target) break;
-                
-                int left = j + 1, right = len - 1;
+                if (nums[i] + nums[j] > 0 && nums[i] + nums[j] > target) break;
+                int left = j + 1, right = nums.size() - 1;
                 while (left < right) {
-                    long sum = (long)twoSum + nums[left] + nums[right];
+                    long sum = (long)nums[i] + nums[j] + nums[left] + nums[right];
                     if (sum == target) {
                         ret.push_back({nums[i],nums[j],nums[left],nums[right]});
                         left++; right--;
                         while (left < right && nums[left] == nums[left - 1]) left++;
-                        while (left < right && nums[right] == nums[right + 1]) right--;
+                        while (left < right && nums[right] == nums[right + 1]) right--; 
                     }
-                    else if (sum > target) 
-                        right--;
-                    else 
-                        left++;
-                }   
+                    else if (sum > target) right--;
+                    else left++;
+                }
             }
         }
         return ret;
