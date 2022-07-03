ac637dae097cc1b8cfcc8a9b0fc6311339bb26a9
seonghwan <76815545+empodi@users.noreply.github.com>
2022-07-03 14:27:22 +0900

    Time: 328 ms (40.80%), Space: 26 MB (37.99%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        string ans = s.substr(0,1);
-        int slen = s.length();
         bool dp[1001][1001];
+        int sLen = s.size();
+        string ans = s.substr(0,1); 
         memset(dp, false, sizeof(dp));
         
-        for (int i = 0; i < slen; i++) {
+        for (int i = 0; i < sLen; i++) {
             dp[i][i] = true;
             if (i) dp[i][i - 1] = true;
         }
         
-        for (int len = 2; len <= slen; len++) {
-            for (int i = 0; i <= slen - len; i++) {
+        for (int len = 2; len <= sLen; len++) {
+            for (int i = 0; i <= sLen - len; i++) {
                 if (s[i] == s[i + len - 1] && dp[i + 1][i + len - 2]) {
-                    dp[i][i + len - 1] = true;
-                    if (ans.length() < len) {
+                    if (ans.size() < len) {
                         ans = s.substr(i, len);
-                        cout << ans;
                     }
+                    dp[i][i + len - 1] = true;
                 }
             }
         }
