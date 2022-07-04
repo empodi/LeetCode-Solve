518e43b642338e934a7a49e17772019f1adf3df4
seonghwan <76815545+empodi@users.noreply.github.com>
2022-07-04 14:28:32 +0900

    Time: 5 ms (27.89%), Space: 6.4 MB (86.06%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -1,31 +1,26 @@
 class Solution {
-public:
-    vector<vector<int>> answer;
-    
-    void solve(vector<int>& v, int cur, int sum, int& k, int& n) {
-        
-        if (sum > n) return;
-        
-        if (v.size() == k) {
-            if (sum == n) 
-                answer.push_back(v);
+private:
+    vector<vector<int>> ret;
+    int target;
+    int len;
+    void solve(vector<int>& v, int cur, int sum) {
+        if (sum > target || cur > target || v.size() > len) return;
+        if (v.size() == len && sum == target) {
+            ret.push_back(v);
             return;
         }
-
-        for (int i = cur; i < min(n, 10); i++) {
-            
+        for (int i = cur; i < 10; i++) {
             v.push_back(i);
-            solve(v, i + 1, sum + i, k, n);
+            solve(v, i + 1, sum + i);
             v.pop_back();
         }
     }
-    
+public:
     vector<vector<int>> combinationSum3(int k, int n) {
-        
         vector<int> v;
-        
-        solve(v, 1, 0, k, n);
-        
-        return answer;   
+        target = n;
+        len = k;
+        solve(v, 1, 0);
+        return ret;
     }
 };
