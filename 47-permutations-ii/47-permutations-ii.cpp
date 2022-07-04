74a2044ce6ea3cd6ab9cb5c84bd4f944e5073e6c
seonghwan <76815545+empodi@users.noreply.github.com>
2022-07-04 14:07:54 +0900

    Time: 0 ms (100.00%), Space: 9 MB (73.29%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,52 +1,26 @@
 class Solution {
-public:
+private:
+    bool check[10];
     vector<vector<int>> ret;
-    bool visited[8];
-    int len;
-    
-    void helper(vector<int>& nums, vector<int>& v) {
-        
-        for (int i = 0; i < len; i++) {
-            
-            if (visited[i]) continue;
-            if (i > 0 && nums[i] == nums[i - 1] && !visited[i - 1])
-                continue;
-            
-            v.push_back(nums[i]);
-            visited[i] = true;
-            
-            if (v.size() == len) ret.push_back(v);
-            else helper(nums, v);
-            
-            v.pop_back();
-            visited[i] = false;
+    void solve(vector<int>& nums, vector<int>& v) {
+        if (nums.size() == v.size()) {
+            ret.push_back(v);
+            return;
+        }
+        for (int i = 0; i < nums.size(); i++) {
+            if (check[i]) continue;
+            v.push_back(nums[i]); check[i] = true;
+            solve(nums, v);
+            v.pop_back(); check[i] = false;
+            while (i < nums.size() - 1 && nums[i] == nums[i + 1]) i++;
         }
     }
-    
+public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-    
-        len = nums.size();
-        
-        if (len == 1) {
-            ret.push_back(nums);
-            return ret;
-        }
-        
-        sort(nums.begin(), nums.end());    
-        
-        for (int i = 0; i < len; i++) {
-            
-            if (i > 0 && nums[i] == nums[i - 1]) 
-                continue;
-
-            memset(visited, false, sizeof(visited));
-            
-            vector<int> v;
-            v.push_back(nums[i]);
-            visited[i] = true;
-            helper(nums, v);
-        }
-        
+        sort(nums.begin(), nums.end());
+        memset(check, false, sizeof(check));
+        vector<int> v;
+        solve(nums, v);
         return ret;
     }
 };
