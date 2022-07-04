784c7e36c41cf15ac24270ece5e8bd3ee2c6fe48
seonghwan <76815545+empodi@users.noreply.github.com>
2022-07-04 14:17:53 +0900

    Time: 2 ms (83.81%), Space: 7.8 MB (56.20%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,28 +1,20 @@
 class Solution {
-public:
+private:
     vector<vector<int>> ret;
-    
-    void solve(vector<int>& nums, vector<int>& v, int idx) {
-        
+    void solve(vector<int>& nums, vector<int>& v, int pos) {
         ret.push_back(v);
-        
-        for (int i = idx; i < nums.size(); i++) {
-            
-            if (i > idx && nums[i] == nums[i - 1]) continue;
-            
+        for (int i = pos; i < nums.size(); i++) {
             v.push_back(nums[i]);
             solve(nums, v, i + 1);
             v.pop_back();
+            while (i < nums.size() - 1 && nums[i] == nums[i + 1]) i++;
         }
     }
-    
+public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        
         sort(nums.begin(), nums.end());
-        
         vector<int> v;
         solve(nums, v, 0);
-        
         return ret;
     }
 };
