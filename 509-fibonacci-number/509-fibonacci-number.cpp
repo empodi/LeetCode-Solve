4a8449a7d7603bec7bcc1b734f4a4a350e8181b2
seonghwan <empodi31@gmail.com>
2022-07-06 10:51:12 +0900

    Time: 0 ms (100.00%), Space: 5.8 MB (96.23%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -2,13 +2,12 @@ class Solution {
 private:
     int dp[40] = { 0, };
 public:
-    /*
     int fib(int n) {
         if (n < 2) return n;
         return (dp[n - 1] ? dp[n - 1] : (dp[n - 1] = fib(n - 1))) +
             (dp[n - 2] ? dp[n - 2] : (dp[n - 2] = fib(n - 2)));
     }
-    */
+    /*
     int fib(int n) {
         for (int i = 0; i <= n; i++) {
             if (i < 2) 
@@ -17,5 +16,5 @@ public:
                 dp[i] = dp[i - 1] + dp[i - 2];
         }
         return dp[n];
-    }
+    } */
 };
