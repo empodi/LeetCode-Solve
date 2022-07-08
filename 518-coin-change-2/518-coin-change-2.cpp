d0ab8735b058b412040150eab72e1e2c754ba376
seonghwan <empodi31@gmail.com>
2022-07-08 22:12:34 +0900

    Time: 91 ms (16.14%), Space: 21.6 MB (7.79%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
-        vector<int> dp(amount + 1, 0);
-        dp[0] = 1;
-        for (auto& c : coins) 
-            for (int i = 1; i <= amount; i++)
-                dp[i] += (i < c ? 0 : dp[i - c]);
-        return dp[amount];
+        vector<vector<int>> dp(amount + 1, vector<int>(coins.size() + 1));
+        for (int i = 0; i <= coins.size(); i++)
+            dp[0][i] = 1;
+        for (int i = 1; i <= amount; i++) { // i stands for current amount
+            for (int j = 1; j <= coins.size(); j++) {
+                dp[i][j] = dp[i][j - 1] + (i < coins[j - 1] ? 0 : dp[i - coins[j - 1]][j]);
+            }
+        }
+        return dp[amount][coins.size()];
     }
 };
