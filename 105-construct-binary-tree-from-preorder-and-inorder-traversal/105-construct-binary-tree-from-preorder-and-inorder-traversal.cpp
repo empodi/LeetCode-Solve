527b3f80e193c906c22649ba6ab321938b1beb38
seonghwan <empodi31@gmail.com>
2022-07-14 11:28:49 +0900

    Time: 11 ms (98.77%), Space: 26 MB (64.01%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -9,14 +9,14 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-inline int idx_convert(int a){return a+3000;}
+//inline int idx_convert(int a){return a+3000;}
 class Solution {
 private:
     int inorder_idx[10000] = { 0, };
     TreeNode* makeTree(vector<int>& preorder, vector<int>& inorder, int ps, int pe, int is, int ie) { 
         if (ps > pe || is > ie) return nullptr;
         int val = preorder[ps];
-        int val_idx = inorder_idx[idx_convert(val)]; // val idx in inorder array
+        int val_idx = inorder_idx[val + 3000]; // val idx in inorder array
 
         TreeNode* node = new TreeNode(val);
         node->left = makeTree(preorder, inorder, ps + 1, ps + val_idx - is, is, val_idx);
@@ -26,7 +26,7 @@ private:
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         for (int i = 0; i < inorder.size(); i++) {
-            inorder_idx[idx_convert(inorder[i])] = i;
+            inorder_idx[inorder[i] + 3000] = i;
         }
         return makeTree(preorder, inorder, 0, preorder.size() - 1, 0, inorder.size() - 1);
     }
