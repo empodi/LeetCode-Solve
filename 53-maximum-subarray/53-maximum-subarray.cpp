64622d1bc6dc599e3d376946894d0b46e22d13ba
seonghwan <empodi31@gmail.com>
2022-07-15 17:12:34 +0900

    Time: 142 ms (70.29%), Space: 70.5 MB (5.93%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,13 +1,13 @@
-inline int bi_max(int a, int b) { return a>b ? a:b; }
+inline int MAX(int a, int b) { return a>b ? a:b; }
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         vector<int> dp(nums.size());
+        int answer = nums[0];
         dp[0] = nums[0];
-        int answer = dp[0];
         for (int i = 1; i < nums.size(); i++) {
-            dp[i] = bi_max(dp[i - 1] + nums[i], nums[i]);
-            answer = bi_max(answer, dp[i]);
+            dp[i] = MAX(dp[i - 1] + nums[i], nums[i]);
+            answer = MAX(answer, dp[i]);
         }
         return answer;
     }
