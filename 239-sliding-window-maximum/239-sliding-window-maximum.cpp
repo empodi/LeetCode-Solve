a4d3206af66a125d6a24656a63e35a1fa1532ac4
seonghwan <empodi31@gmail.com>
2022-08-09 11:59:31 +0900

    Time: 395 ms (74.23%), Space: 148.9 MB (23.69%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -1,41 +1,18 @@
 class Solution {
 public:
-    int numsLen;
-    int counter[20002] = { 0 };
-    
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> v;
+        priority_queue<pair<int,int>> pq;
         
-        numsLen = nums.size();
-        priority_queue<int> pq;
-        
-        vector<int> answer;
-        int left = 0, right = k - 1;
-        
-        while (right < numsLen) {
+        for (int i = 0; i < nums.size(); i++) {
+            pq.push({ nums[i], i });
             
-            if (left == 0) {
-                
-                for (int i = left; i <= right; i++) {
-                    counter[nums[i] + 10000]++;
-                    pq.push(nums[i]);
-                }
+            if (i >= k - 1) {
+                while (!pq.empty() && pq.top().second <= i - k)
+                    pq.pop();
+                v.push_back(pq.top().first);
             }
-            else {
-                
-                counter[nums[left - 1] + 10000]--;
-                counter[nums[right] + 10000]++;
-                pq.push(nums[right]);
-            }
-            
-            while (!pq.empty() && counter[pq.top() + 10000] == 0) {
-                pq.pop();
-            }
-            
-            answer.push_back(pq.top());
-            left++;
-            right++;
         }
-        
-        return answer;
+        return v;
     }
 };
