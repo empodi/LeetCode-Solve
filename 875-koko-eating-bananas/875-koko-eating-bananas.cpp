3df22a8154cccbe0096b4bff4e75a3fb698f8e9b
seonghwan <empodi31@gmail.com>
2022-09-21 14:33:06 +0900

    Time: 100 ms (31.26%), Space: 18.9 MB (33.40%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.cpp
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -1,33 +1,17 @@
 class Solution {
 public:
-    bool checkTime(vector<int>& piles, int h, int limit) {
-        
-        int hourCnt = 0;
-        
-        for (auto& n : piles) {
-            
-            hourCnt += n / h;
-            if (n % h) hourCnt++;
-            
-            if (hourCnt > limit) return false;
-        }
-        return true;
+    long long check(const vector<int>& piles, const int& m) {
+        long long ret = 0;
+        for (auto& p : piles) ret += (p % m == 0 ? p / m : p / m + 1);
+        return ret;
     }
-    
     int minEatingSpeed(vector<int>& piles, int h) {
-        
-        int left = 1, right = *max_element(piles.begin(), piles.end());
-        
-        if (h == piles.size()) return right;
-        
-        while (left <= right) {
-            
-            int mid = (left + right) / 2;
-            
-            if (checkTime(piles, mid, h)) right = mid - 1;
-            else left = mid + 1;
+        int s = 1, e = 1e9;
+        while (s <= e) {
+            int mid = (s + e) / 2;
+            if (check(piles, mid) <= h) e = mid - 1;
+            else s = mid + 1;
         }
-        
-        return left;
+        return s;
     }
 };
