3ec3eaed73dd63c7312f893b022ffab9e066cbad
seonghwan <empodi31@gmail.com>
2022-09-21 12:36:27 +0900

    Time: 12 ms (57.10%), Space: 13.2 MB (30.01%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -4,8 +4,7 @@ public:
         vector<int> dp(prices.size(),0);
         int curmin = prices[0];
         for (int i = 1; i < prices.size(); i++) {
-            dp[i] = max(prices[i] - prices[i - 1] + dp[i - 1], prices[i] - curmin);
-            dp[i] = max(dp[i], dp[i - 1]);
+            dp[i] = max(dp[i - 1], max(prices[i] - curmin, prices[i] - prices[i - 1] + dp[i - 1]));
             curmin = min(curmin, prices[i]);
         }
         return dp.back();
